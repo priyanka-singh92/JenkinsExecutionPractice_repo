7b1907cb4981dc89d4acc04c8f746a380e36dc28
Piku <Piku@192.168.148.118>
2025-04-19 10:44:45 +0530

    first commit in Jenkins

---
+++ JenkinsExecutionPractice/src/test/java/demo/JenkinsTest.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	
+	@Test
+	
+	public void launch()
+	{
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://demowebshop.tricentis.com/");
+		Reporter.log("DemoWebShop launched successfully",true);
+	}
+
+}
